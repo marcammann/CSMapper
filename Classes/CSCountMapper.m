b85a20f469ea3b1fb578a333996062987deb5016
Marc Ammann <mammann@hugeinc.com>
2012-12-27 01:08:45 -0800

    added assertions, removed log output

--- Classes/CSCountMapper.m
+++ Classes/CSCountMapper.m
@@ -11,12 +11,7 @@
 @implementation CSCountMapper
 
 + (NSNumber *)transformValue:(NSArray *)inputValue {
-	if (![inputValue isKindOfClass:[NSArray class]]) {
-#ifdef DEBUG
-		LOG_G(@"[CSCountMapper Mapping Error]: %@ is not of type NSArray", inputValue);
-#endif
-		return nil;
-	}
+	NSAssert([inputValue isKindOfClass:[NSArray class]], @"Input Value needs to be Array");
 	
 	return [NSNumber numberWithInt:[inputValue count]];
 }
--- Classes/CSJoinMapper.m
+++ Classes/CSJoinMapper.m
@@ -11,13 +11,8 @@
 @implementation CSJoinMapper
 
 + (NSString *)transformValue:(NSArray *)inputValue {
-	if (![inputValue isKindOfClass:[NSArray class]]) {
-#ifdef DEBUG
-		LOG_G(@"[Mapping Error]: %@ is not of type NSArray", inputValue);
-#endif
-		return nil;
-	}
-	
+	NSAssert([inputValue isKindOfClass:[NSArray class]], @"Input Value needs to be Array");
+
 	return [inputValue componentsJoinedByString:@":"];
 }
 
